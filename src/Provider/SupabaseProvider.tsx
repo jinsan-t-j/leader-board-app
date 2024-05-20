34a19fd72ff7b7ae7a80093f88811393b3d44cbe
Jinsan T J <hellojinsantj@gmail.com>
2024-05-20 13:24:56 +0530

    disabled supabase

--- src/Provider/SupabaseProvider.tsx
+++ src/Provider/SupabaseProvider.tsx
@@ -1,5 +1,5 @@
 import type { FC, ReactNode } from 'react'
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
 import { SupabaseContext } from './SupabaseContext'
 
@@ -10,10 +10,10 @@ interface ISupabaseProvider {
 /**
  * The supabase client.
  */
-const supabaseClient = createClient(
-    import.meta.env.VITE_SUPABASE_PROJECT_URL,
-    import.meta.env.VITE_SUPABASE_API_KEY,
-)
+// const supabaseClient = createClient(
+//     import.meta.env.VITE_SUPABASE_PROJECT_URL,
+//     import.meta.env.VITE_SUPABASE_API_KEY,
+// )
 
 /**
  * The supabase provider for the application.
@@ -21,5 +21,5 @@ const supabaseClient = createClient(
  * @returns The Supabase context provider.
  */
 export const SupabaseProvider: FC<ISupabaseProvider> = ({ children }) => {
-    return <SupabaseContext.Provider value={supabaseClient}> {children} </SupabaseContext.Provider>
+    return <SupabaseContext.Provider value={null}> {children} </SupabaseContext.Provider>
 }
