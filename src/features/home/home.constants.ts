388ee6fd97bff63d22dc4980c02f812bc995c4dd
Jinsan T J <hellojinsantj@gmail.com>
2024-05-22 02:46:36 +0530

    avatar constant

---
+++ src/features/home/home.constants.ts
@@ -0,0 +1,13 @@
+/**
+ * The avatar theme constant for the application
+ */
+export const AvatarThemeConstant = {
+    root: {
+        bordered: 'ring-4',
+        rounded: 'rounded-full',
+        color: {
+            yellow: 'ring-yellow-700',
+            white: 'ring-white',
+        },
+    },
+}
