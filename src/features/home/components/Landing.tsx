fad6f1ba3c40036163d8d694d87d1cf05f2ced6b
Jinsan T J <hellojinsantj@gmail.com>
2024-05-26 15:21:11 +0530

    animation

--- src/features/home/components/Landing.tsx
+++ src/features/home/components/Landing.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState, type FC } from 'react'
-import { Avatar } from 'flowbite-react'
+import { useEffect, useState, type FC } from "react"
+import { Avatar } from "flowbite-react"
 
-import bgLines from '../../../assets/images/bg-lines.png'
-import { AvatarThemeConstant } from '../home.constants'
-import { LightningIcon } from '../../../shared/icons/LightningIcon'
-import { useAppSelector } from '../../../store'
-import type { ILeader } from '../entity/Leader'
+import bgLines from "../../../assets/images/bg-lines.png"
+import { AvatarThemeConstant } from "../home.constants"
+import { LightningIcon } from "../../../shared/icons/LightningIcon"
+import { useAppSelector } from "../../../store"
+import type { ILeader } from "../entity/Leader"
 
 /**
  * The landing component of the application
@@ -13,80 +13,85 @@ import type { ILeader } from '../entity/Leader'
  * @returns The JSX.
  */
 export const Landing: FC = () => {
-    const [topThreeLeaders, setTopThreeLeaders] = useState<ILeader[]>()
+  const [topThreeLeaders, setTopThreeLeaders] = useState<ILeader[]>()
 
-    const data = useAppSelector((store) => store.leaders)
+  const data = useAppSelector(store => store.leaders)
 
-    const initialData = () => {
-        const leaders = data?.slice(0, 3)
+  const initialData = () => {
+    const leaders = data?.slice(0, 3)
 
-        setTopThreeLeaders(leaders)
-    }
+    setTopThreeLeaders(leaders)
+  }
 
-    useEffect(() => {
-        initialData()
-    }, [data])
+  useEffect(() => {
+    initialData()
+  }, [data])
 
-    return (
-        <div className='relative flex flex-col justify-center md:w-3/5'>
-            <h2 className='-mb-1 font-light text-2xl text-black max-lg:mt-8 sm:-mb-2 md:-mb-8 md:text-3xl xl:text-4xl'>
-                Use your time wisely
-            </h2>
-            <h1 className='mb-16 font-light text-4xl tracking-wide text-black md:text-6xl xl:text-7xl'>
-                You Are A{' '}
-                <span className='font-semi-bold text-6xl md:text-8xl xl:text-9xl'> Star </span>
-            </h1>
-            <div className='flex justify-start max-md:flex-col'>
-                <div className='flex justify-start max-sm:flex-col'>
-                    {topThreeLeaders?.map((leader, index) => {
-                        const targetPercentage = leader.target_percentage
-                        let percentageGradient: string = `${targetPercentage} - CHECK DATABASE`
-                        if (targetPercentage < 30) {
-                            percentageGradient = 'from-red-700 to-yellow-700'
-                        } else if (targetPercentage < 60) {
-                            percentageGradient = 'from-red-700 via-orange-400 to-yellow-700'
-                        } else if (targetPercentage < 100) {
-                            percentageGradient = 'from-red-700 via-orange-400 to-green-700'
-                        }
+  return (
+    <div className="relative flex flex-col justify-center md:w-3/5">
+      <h2 className="-mb-1 font-light text-2xl text-black max-lg:mt-8 sm:-mb-2 md:-mb-8 md:text-3xl xl:text-4xl">
+        Use your time wisely
+      </h2>
+      <h1 className="mb-16 font-light text-4xl tracking-wide text-black md:text-6xl xl:text-7xl">
+        You Are A{" "}
+        <span className="font-semi-bold text-6xl md:text-8xl xl:text-9xl">
+          {" "}
+          Star{" "}
+        </span>
+      </h1>
+      <div className="flex justify-start max-md:flex-col">
+        <div className="flex justify-start max-sm:flex-col">
+          {topThreeLeaders?.map((leader, index) => {
+            const targetPercentage = leader.target_percentage
+            let percentageGradient: string = `${targetPercentage} - CHECK DATABASE`
+            if (targetPercentage < 30) {
+              percentageGradient = "from-red-700 to-yellow-700"
+            } else if (targetPercentage < 60) {
+              percentageGradient = "from-red-700 via-orange-400 to-yellow-700"
+            } else if (targetPercentage < 100) {
+              percentageGradient = "from-red-700 via-orange-400 to-green-700"
+            }
 
-                        return (
-                            <div
-                                className={`mr-2 flex flex-col items-center justify-center max-lg:mb-16 md:mr-5 ${index !== 2 ? 'lg:mr-10' : ''}`}
-                                key={leader.uuid}
-                            >
-                                <Avatar
-                                    img={leader.avatar}
-                                    rounded
-                                    bordered
-                                    size='lg'
-                                    theme={AvatarThemeConstant}
-                                    color='yellow'
-                                    className='mb-8'
-                                />
-                                <div className='mb-1 h-3.5 w-16 rounded-3xl bg-black' />
-                                <div className='mb-4 flex flex-col justify-center rounded-2xl bg-black p-4 py-8'>
-                                    <div
-                                        // eslint-disable-next-line tailwindcss/no-custom-classname
-                                        className={`lightning bg-gradient-to-t ${percentageGradient} delay-150 duration-200`}
-                                    />
-                                </div>
-                                <h4 className='text-center font-semi-bold text-lg'>
-                                    {leader.name}
-                                </h4>
-                            </div>
-                        )
-                    })}
+            return (
+              <div
+                className={`mr-2 flex flex-col items-center justify-center max-lg:mb-16 md:mr-5 ${index !== 2 ? "lg:mr-10" : ""}`}
+                key={leader.uuid}
+              >
+                <Avatar
+                  img={leader.avatar}
+                  rounded
+                  bordered
+                  size="lg"
+                  theme={AvatarThemeConstant}
+                  color="yellow"
+                  className="mb-8"
+                />
+                <div className="mb-1 h-3.5 w-16 rounded-3xl bg-black" />
+                <div className="mb-4 flex flex-col justify-center rounded-2xl bg-black p-4 py-8">
+                  <div className="relative lightning-container">
+                    <div
+                      className={`absolute bottom-0 w-full border   lightning  bg-gradient-to-t ${percentageGradient} delay-150 duration-200`}
+                      style={{ height: `${targetPercentage}%` }}
+                    />
+                  </div>
                 </div>
-                <button
-                    className='m-auto flex size-16 items-center justify-center self-center rounded-full bg-black max-md:mt-6'
-                    onClick={() => location.reload()}
-                >
-                    <LightningIcon />
-                </button>
-            </div>
-            <div className='-right-36 top-0 -z-10 max-md:hidden md:absolute'>
-                <img src={bgLines} alt='The background lines' />
-            </div>
+                <h4 className="text-center font-semi-bold text-lg">
+                  {leader.name}
+                </h4>
+              </div>
+            )
+          })}
         </div>
-    )
+        <button
+          className="m-auto flex size-16 items-center justify-center self-center rounded-full bg-black max-md:mt-6"
+          onClick={() => location.reload()}
+        >
+          <LightningIcon />
+        </button>
+      </div>
+      <div className="-right-36 top-0 -z-10 max-md:hidden md:absolute">
+        <img src={bgLines} alt="The background lines" />
+      </div>
+    </div>
+  )
 }
--- src/features/home/components/LeaderModal.tsx
+++ src/features/home/components/LeaderModal.tsx
@@ -1,15 +1,15 @@
-import type { FC } from 'react'
-import { useEffect, useState } from 'react'
-import type { Styles } from 'react-modal'
-import Modal from 'react-modal'
-import { Avatar } from 'flowbite-react'
+import type { FC } from "react"
+import { useEffect, useState } from "react"
+import type { Styles } from "react-modal"
+import Modal from "react-modal"
+import { Avatar } from "flowbite-react"
 
-import confetti from '../../../assets/images/confetti.png'
-import type { ILeader } from '../entity/Leader'
-import { useAppSelector } from '../../../store'
-import '.././../../assets/sass/features/home/_leaderModal.scss'
+import confetti from "../../../assets/images/confetti.png"
+import type { ILeader } from "../entity/Leader"
+import { useAppSelector } from "../../../store"
+import ".././../../assets/sass/features/home/_leaderModal.scss"
 
-Modal.setAppElement('#root')
+Modal.setAppElement("#root")
 
 /**
  * The Leader modal component
@@ -17,80 +17,118 @@ Modal.setAppElement('#root')
  * @returns The JSX.
  */
 export const LeaderModal: FC = () => {
-    const [topLeader, setTopLeader] = useState<ILeader>()
-    const [isOpen, setIsOpen] = useState<boolean>(true)
+  const [topLeader, setTopLeader] = useState<ILeader>()
+  const [isOpen, setIsOpen] = useState<boolean>(true)
+  const [isConfettiVisible, setIsConfettiVisible] = useState<boolean>(false)
 
-    const leaders = useAppSelector((store) => store.leaders)
+  const leaders = useAppSelector(store => store.leaders)
 
-    const avatarTheme = {
-        root: {
-            bordered: 'ring-2 p-1',
-            color: {
-                yellow: 'ring-yellow-700',
-            },
-            img: {
-                base: 'avatar-image rounded ring-4 ring-black',
-            },
-        },
+  const avatarTheme = {
+    root: {
+      bordered: "ring-2 p-1",
+      color: {
+        yellow: "ring-yellow-700",
+      },
+      img: {
+        base: "avatar-image rounded ring-4 ring-black",
+      },
+    },
+  }
+
+  const customStyles: Styles = {
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: "rgba(0, 0, 0, 0.75)",
+    },
+    content: {
+      position: "absolute",
+      inset: "",
+      transform: "translate(-50%, 50%)",
+      width: "450px",
+      height: "300px",
+      borderRadius: "53px",
+      border: "1px solid #ccc",
+      background: "#fff",
+      padding: "20px",
+      outline: "none",
+      overflow: "hidden",
+      WebkitOverflowScrolling: "touch",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  }
+
+  useEffect(() => {
+    setTopLeader(leaders?.[0])
+  }, [leaders])
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        setIsConfettiVisible(true)
+      }, 700) // This should match the modal's transition time
+    } else {
+      setIsConfettiVisible(false)
     }
+  }, [isOpen])
 
-    const customStyles: Styles = {
-        overlay: {
-            position: 'fixed',
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            backgroundColor: 'rgba(0, 0, 0, 0.75)',
-        },
-        content: {
-            position: 'absolute',
-            inset: '',
-            transform: 'translate(-50%, 50%)',
-            width: '30%',
-            height: 'auto',
-            borderRadius: '53px',
-            border: '1px solid  #ccc',
-            background: '#fff',
-            padding: '20px',
-            outline: 'none',
-            overflow: 'hidden',
-            WebkitOverflowScrolling: 'touch',
-        },
-    }
-
-    useEffect(() => {
-        setTopLeader(leaders?.[0])
-    }, [leaders])
-
-    return (
-        <Modal
-            isOpen={isOpen}
-            style={customStyles}
-            closeTimeoutMS={700}
-            shouldCloseOnEsc={true}
-            shouldCloseOnOverlayClick={true}
-            onRequestClose={() => setIsOpen(false)}
-        >
-            <div className='absolute top-0 translate-x-full'>
-                <img src={confetti} alt='The confetti' />
-            </div>
-            <div className='flex flex-col justify-center'>
-                <div className='m-auto w-1/4'>
-                    <Avatar
-                        img={topLeader?.avatar}
-                        size='lg'
-                        color='black'
-                        bordered
-                        rounded
-                        theme={avatarTheme}
-                    />
-                </div>
-                <h3 className='text-center font-semi-bold text-2xl text-black'>
-                    {topLeader?.name}
-                </h3>
-                <h2 className='text-center font-bold text-4xl text-green-700'>Congratulations</h2>
-            </div>
-        </Modal>
-    )
+  return (
+    <Modal
+      isOpen={isOpen}
+      style={customStyles}
+      closeTimeoutMS={700}
+      shouldCloseOnEsc={true}
+      shouldCloseOnOverlayClick={true}
+      onRequestClose={() => setIsOpen(false)}
+    >
+      <style>
+        {`
+          @keyframes popup {
+            0% {
+              transform: scale(0);
+              opacity: 0;
+            }
+            50% {
+              transform: scale(1.21);
+              opacity: 1;
+            }
+            100% {
+              transform: scale(1);
+            }
+          }
+          .animate-popup {
+            animation: popup 0.5s ease-in-out forwards;
+          }
+        `}
+      </style>
+      {isConfettiVisible && (
+        <div className=" absolute top-[-20px] translate-x-full animate-popup">
+          <img src={confetti} alt="The confetti" />
+        </div>
+      )}
+      <div className="flex flex-col justify-center">
+        <div className="m-auto w-1/2">
+          <Avatar
+            img={topLeader?.avatar}
+            size="lg"
+            color="black"
+            bordered
+            rounded
+            theme={avatarTheme}
+          />
+        </div>
+        <h3 className="text-center font-semi-bold text-2xl text-black">
+          {topLeader?.name}
+        </h3>
+        <h2 className="text-center font-bold text-4xl text-green-700">
+          Congratulations
+        </h2>
+      </div>
+    </Modal>
+  )
 }
